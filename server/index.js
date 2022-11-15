9b21e05291bd5a51571e29df3ed840dfdb111aa8
viny <viny0698@gmail.com>
2022-11-15 21:27:52 +0530

    for heroku error

--- server/index.js
+++ server/index.js
@@ -113,6 +113,19 @@ app.use(sessions({
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// ----------- deploy ------------- //
+const _dirname1 = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(_dirname1, '/client/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(_dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
+// ------------- deploy ------------- //
 app.use('/', express.static(path.join(__dirname, '/public/mainPage')))
 
 app.use('/static/login_page', 
