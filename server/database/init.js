5d2a4e2707a18786627d6f726b463563a16a7b7e
viny <viny0698@gmail.com>
2022-11-15 23:23:21 +0530

    heroku deployment

--- server/database/init.js
+++ server/database/init.js
@@ -2,10 +2,12 @@ const { Pool } = require('pg')
 const { createUsersTable, createMessagesTable, createRoomsTable } = require('./queries')
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  // connectionString: process.env.DATABASE_URL,
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/chat_app',
+    ssl: process.env.DATABASE_URL ? true : false
   // user: 'postgres',
   // host: 'localhost',
   // database: 'chat_app',
